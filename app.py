464f2db65cecf2e2794096529b49cb1523db6458
Ikenna Nwankpa <105177758+Ikenna-The-Data-Guy@users.noreply.github.com>
2022-12-09 18:32:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def load_lottieurl(url):
 
 # Use Local CSS
 def local_css(file_name):
-    with open('file_name','r') as f:
+    with open(file_name) as f:
         st.markdown(f"<style>{f.read()}<\style>", unsafe_allow_html=True)
 
 local_css(r"https://raw.githubusercontent.com/Ikenna-The-Data-Guy/website/main/style.css")
